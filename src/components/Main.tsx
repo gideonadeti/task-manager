38e8c06679edc70c656ea6ab8411a6d657c5ff99
gideonadeti <gideonadeti0@gmail.com>
2024-09-24 17:13:30 +0000

    Redirect users to sign in page if they're not authenticated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Sidebar } from "./Sidebar";
 import { Tasks } from "./Tasks";
 import { SidebarToggler } from "./SidebarToggler";
+import { useUserStore } from "../stores/user";
 
 export function Main() {
   const [showSidebar, setShowSidebar] = useState(true);
+
+  const { user } = useUserStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/auth/sign-in");
+    }
+  }, [user, navigate]);
 
   return (
     <div className="position-relative">
