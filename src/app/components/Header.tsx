2d17d7bf59a04f786166fdf857f88739bd0db525
gideonadeti <gideonadeti0@gmail.com>
2024-10-25 18:54:16 +0000

    Initialize shadcn

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Header() {
+  return (
+    <header className="flex items-center border-b py-2 px-4">
+      <Link href="/" className="text-lg">
+        Task Manager
+      </Link>
+      <div className="ms-auto">
+        <UserButton showName />
+      </div>
+    </header>
+  );
+}
