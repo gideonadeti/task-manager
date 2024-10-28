d9548165526ea137159a02cb661808eb5c952668
gideonadeti <gideonadeti0@gmail.com>
2024-10-28 17:59:47 +0000

    Set up group page for displaying fetched data

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -1,14 +1,31 @@
-import { H1, H2, H3, H4, H5, H6 } from "@/app/ui/HTags";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useUser } from "@clerk/nextjs";
+
+import { fetchGroups } from "@/app/query-functions";
+import Spinner from "@/app/components/Spinner";
+import { Group } from "@prisma/client";
 
 export default function GroupPage() {
+  const { user } = useUser();
+  const {
+    status,
+    data: groups,
+    error,
+  } = useQuery({
+    queryKey: ["groups"],
+    queryFn: () => fetchGroups(user!.id),
+  });
+
   return (
     <div className="flex-grow p-2">
-      <H1>Group Page</H1>
-      <H2>Group Page</H2>
-      <H3>Group Page</H3>
-      <H4>Group Page</H4>
-      <H5>Group Page</H5>
-      <H6>Group Page</H6>
+      <div className="flex items-center justify-center">
+        {status === "pending" && <Spinner />}
+        {status === "error" && <h1>{error.message}</h1>}
+      </div>
+      {groups?.length > 0 &&
+        groups.map((group: Group) => <h3 key={group.id}>{group.name}</h3>)}
     </div>
   );
 }
