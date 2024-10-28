bc67b8ce7e5a8be725e0b86ce9608bb66792437a
gideonadeti <gideonadeti0@gmail.com>
2024-10-28 14:39:05 +0000

    Modify HTags using shadcn typography

--- src/app/ui/HTags.tsx
+++ src/app/ui/HTags.tsx
@@ -4,25 +4,41 @@ interface HeadingProps {
 }
 
 export function H1({ children, className = "" }: HeadingProps) {
-  return <h1 className={`text-5xl font-bold ${className}`}>{children}</h1>;
+  return (
+    <h1
+      className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${className}`}
+    >
+      {children}
+    </h1>
+  );
 }
 
 export function H2({ children, className = "" }: HeadingProps) {
-  return <h2 className={`text-4xl font-bold ${className}`}>{children}</h2>;
+  return (
+    <h2
+      className={`scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 ${className}`}
+    >
+      {children}
+    </h2>
+  );
 }
 
 export function H3({ children, className = "" }: HeadingProps) {
-  return <h3 className={`text-3xl font-bold ${className}`}>{children}</h3>;
+  return (
+    <h3
+      className={`scroll-m-20 text-2xl font-semibold tracking-tight ${className}`}
+    >
+      {children}
+    </h3>
+  );
 }
 
 export function H4({ children, className = "" }: HeadingProps) {
-  return <h4 className={`text-2xl font-bold ${className}`}>{children}</h4>;
-}
-
-export function H5({ children, className = "" }: HeadingProps) {
-  return <h5 className={`text-xl font-bold ${className}`}>{children}</h5>;
-}
-
-export function H6({ children, className = "" }: HeadingProps) {
-  return <h6 className={`text-lg font-bold ${className}`}>{children}</h6>;
+  return (
+    <h4
+      className={`scroll-m-20 text-xl font-semibold tracking-tight ${className}`}
+    >
+      {children}
+    </h4>
+  );
 }
