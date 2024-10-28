70a3b3d6be021d2e5b4125914ec1f4d587f1a673
gideonadeti <gideonadeti0@gmail.com>
2024-10-28 17:58:37 +0000

    Create route for handling get requests on

---
+++ src/app/api/groups/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { readGroups } from "../../../../prisma/db";
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const userId = searchParams.get("userId");
+
+  if (!userId) {
+    return NextResponse.json({ error: "userId is required" }, { status: 400 });
+  }
+
+  try {
+    const groups = await readGroups(userId);
+
+    return NextResponse.json({ groups }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Something went wrong while reading groups." },
+      { status: 500 }
+    );
+  }
+}
