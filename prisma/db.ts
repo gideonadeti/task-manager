6f1fc2c13d6232b368cef655474e8b3e350b12c6
gideonadeti <gideonadeti0@gmail.com>
2024-10-28 17:56:04 +0000

    Create module for querying db

---
+++ prisma/db.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
+
+export async function readGroups(userId: string) {
+  try {
+    const groups = await prismaClient.group.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+
+    // If there are no groups, create and return default group as list
+    if (groups.length === 0) {
+      const group = await createGroup("Inbox", userId);
+
+      return [group];
+    }
+
+    return groups;
+  } catch (error) {
+    console.error("Error reading groups:", error);
+
+    throw error;
+  }
+}
+
+export async function createGroup(name: string, userId: string) {
+  try {
+    const group = await prismaClient.group.create({
+      data: {
+        name,
+        userId,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+
+    return group;
+  } catch (error) {
+    console.error("Error creating group:", error);
+
+    throw error;
+  }
+}
