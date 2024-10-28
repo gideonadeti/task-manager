c12920777510f812178db8e1386a7488c833340c
gideonadeti <gideonadeti0@gmail.com>
2024-10-28 15:53:31 +0000

    Modify custom tags

--- src/app/ui/CustomTags.tsx
+++ src/app/ui/CustomTags.tsx
@@ -1,11 +1,9 @@
-interface HeadingProps {
+interface TagProps {
   children: React.ReactNode;
   className?: string;
 }
 
-interface ParagraphProps extends HeadingProps {}
-
-export function H1({ children, className = "" }: HeadingProps) {
+export function H1({ children, className = "" }: TagProps) {
   return (
     <h1
       className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${className}`}
@@ -15,40 +13,36 @@ export function H1({ children, className = "" }: HeadingProps) {
   );
 }
 
-export function H2({ children, className = "" }: HeadingProps) {
+export function H2({ children, className = "" }: TagProps) {
   return (
     <h2
-      className={`scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 ${className}`}
+      className={`scroll-m-20 text-3xl font-semibold tracking-tight lg:text-4xl ${className}`}
     >
       {children}
     </h2>
   );
 }
 
-export function H3({ children, className = "" }: HeadingProps) {
+export function H3({ children, className = "" }: TagProps) {
   return (
     <h3
-      className={`scroll-m-20 text-2xl font-semibold tracking-tight ${className}`}
+      className={`scroll-m-20 text-2xl font-semibold tracking-tight lg:text-3xl ${className}`}
     >
       {children}
     </h3>
   );
 }
 
-export function H4({ children, className = "" }: HeadingProps) {
+export function H4({ children, className = "" }: TagProps) {
   return (
     <h4
-      className={`scroll-m-20 text-xl font-semibold tracking-tight ${className}`}
+      className={`scroll-m-20 text-xl font-semibold tracking-tight lg:text-2xl ${className}`}
     >
       {children}
     </h4>
   );
 }
 
-export function P({ children, className = "" }: ParagraphProps) {
-  return (
-    <p className={`leading-7 [&:not(:first-child)]:mt-6 ${className}`}>
-      {children}
-    </p>
-  );
+export function P({ children, className = "" }: TagProps) {
+  return <p className={`leading-7 ${className}`}>{children}</p>;
 }
