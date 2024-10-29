8ec6ead8d33c8699198f6bdb7812c3ed100268f1
Gideon Adeti <gideonadeti0@gmail.com>
2024-10-29 18:22:50 +0000

    Set up route to handle get requests at /tasks endpoint

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { readTasks } from "../../../../prisma/db";
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const userId = searchParams.get("userId");
+
+  if (!userId) {
+    return NextResponse.json({ error: "userId is required" }, { status: 400 });
+  }
+
+  try {
+    const tasks = await readTasks(userId);
+
+    return NextResponse.json({ tasks }, { status: 500 });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Something went wrong while reading tasks." },
+      { status: 500 }
+    );
+  }
+}
