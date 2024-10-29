a0183df74744acae1b09ccfc4c1c1ddf08f84cf5
Gideon Adeti <gideonadeti0@gmail.com>
2024-10-29 18:23:41 +0000

    Fetch tasks on mount and handle errors gracefully

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -2,30 +2,66 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { useUser } from "@clerk/nextjs";
+import { useToast } from "@/hooks/use-toast";
+import { AxiosError } from "axios";
 
-import { readGroups } from "@/app/query-functions";
+import { readGroups, readTasks } from "@/app/query-functions";
 import Spinner from "@/app/components/Spinner";
-import { Group } from "@prisma/client";
+import { Group, Task } from "@prisma/client";
+import { useEffect } from "react";
 
 export default function GroupPage() {
   const { user } = useUser();
+  const { toast } = useToast();
   const {
-    status,
+    status: groupsStatus,
     data: groups,
-    error,
-  } = useQuery({
+    error: groupsError,
+  } = useQuery<Group[], AxiosError>({
     queryKey: ["groups"],
     queryFn: () => readGroups(user!.id),
   });
+  const {
+    status: tasksStatus,
+    data: tasks,
+    error: tasksError,
+  } = useQuery<Task[], AxiosError>({
+    queryKey: ["tasks"],
+    queryFn: () => readTasks(user!.id),
+  });
+
+  useEffect(() => {
+    if (groupsStatus === "error") {
+      const errorMessage = (groupsError?.response?.data as { error: string })
+        .error;
+      toast({
+        description: errorMessage || "Something went wrong",
+        variant: "destructive",
+      });
+    }
+
+    if (tasksStatus === "error") {
+      const errorMessage = (tasksError?.response?.data as { error: string })
+        .error;
+      toast({
+        description: errorMessage || "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  }, [groupsError, groupsStatus, tasksError, tasksStatus, toast]);
 
   return (
     <div className="flex-grow p-2">
       <div className="flex items-center justify-center">
-        {status === "pending" && <Spinner />}
-        {status === "error" && <h1>{error.message}</h1>}
+        {(groupsStatus === "pending" || tasksStatus === "pending") && (
+          <Spinner />
+        )}
       </div>
-      {groups?.length > 0 &&
-        groups.map((group: Group) => <h3 key={group.id}>{group.name}</h3>)}
+
+      {groups && groups.length > 0 && (
+        <div>Number of groups: {groups.length}</div>
+      )}
+      {tasks && tasks.length > 0 && <div>Number of tasks: {tasks.length}</div>}
     </div>
   );
 }
