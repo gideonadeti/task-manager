6bd42d3764334ab285642937d46605802639e488
Gideon Adeti <gideonadeti0@gmail.com>
2024-10-31 04:12:41 +0000

    Create function for formatting dates

---
+++ src/app/format-date.ts
@@ -0,0 +1,24 @@
+import {
+  isToday,
+  isYesterday,
+  isTomorrow,
+  formatDistanceToNow,
+  format,
+  isThisYear,
+} from "date-fns";
+
+export default function formatDate(date: Date) {
+  const distanceToNow = formatDistanceToNow(date, { addSuffix: true });
+
+  if (isToday(date)) {
+    return `${format(date, "h:mm a")} (${distanceToNow})`;
+  } else if (isTomorrow(date)) {
+    return `Tomorrow at ${format(date, "h:mm a")} (${distanceToNow})`;
+  } else if (isYesterday(date)) {
+    return `Yesterday at ${format(date, "h:mm a")} (${distanceToNow})`;
+  } else if (isThisYear(date)) {
+    return `${format(date, "eee, do MMM")} (${distanceToNow})`;
+  } else {
+    return `${format(date, "eee, do MMM, yyyy")} (${distanceToNow})`;
+  }
+}
