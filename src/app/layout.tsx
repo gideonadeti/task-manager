ae0e858ac9d647a5f9e4d7f005ed1827204a708b
Gideon Adeti <gideonadeti0@gmail.com>
2024-10-31 04:12:58 +0000

    Set overflow-y to hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} overflow-y-hidden`}>
         <ClerkProvider>
           <SignedIn>
             <QCProvider>
