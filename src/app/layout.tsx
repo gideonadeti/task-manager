7214a5902679e5df05acda6578863655b03a2b55
gideonadeti <gideonadeti0@gmail.com>
2025-04-04 19:30:10 +0000

    Enable rich colors for Toaster component in RootLayout for enhanced visual feedback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
               >
                 {children}
               </ThemeProvider>
-              <Toaster />
+              <Toaster richColors />
             </QCProvider>
           </SignedIn>
           <SignedOut>
