6b021a43df154db73f51498a7a43cf498eb59202
gideonadeti <gideonadeti0@gmail.com>
2025-04-04 19:22:18 +0000

    Enhance NoTasks component to display personalized messages based on group context and user first name

--- src/app/groups/[groupId]/components/no-tasks.tsx
+++ src/app/groups/[groupId]/components/no-tasks.tsx
@@ -1,8 +1,12 @@
+import { useUser } from "@clerk/nextjs";
+
 interface NoTasksProps {
   groupId: string;
 }
 
 const NoTasks = ({ groupId }: NoTasksProps) => {
+  const { user } = useUser();
+
   switch (groupId) {
     case "inbox":
       return (
@@ -12,8 +16,62 @@ const NoTasks = ({ groupId }: NoTasksProps) => {
         </div>
       );
 
+    case "today":
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">
+            You&#39;re all done for today, {user?.firstName}
+          </span>
+          <p>Enjoy the rest of your day!</p>
+        </div>
+      );
+    case "tomorrow":
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">
+            You&#39;re all set for tomorrow, {user?.firstName}
+          </span>
+          <p>No tasks are due tomorrow.</p>
+        </div>
+      );
+
+    case "this-week":
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">
+            You&#39;re all set for this week, {user?.firstName}
+          </span>
+          <p>No tasks are due this week.</p>
+        </div>
+      );
+
+    case "overdue":
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">
+            You&#39;re all caught up, {user?.firstName}
+          </span>
+          <p>No overdue tasks.</p>
+        </div>
+      );
+
+    case "completed":
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">
+            You&#39;re a task-crushing machine, {user?.firstName}
+          </span>
+          <p>Nothing to see here.</p>
+        </div>
+      );
+
     default:
-      return null;
+      return (
+        <div className="flex flex-col items-center justify-center h-full p-4 text-center">
+          <span className="font-semibold">No tasks here yet</span>
+          <p>Create a task to get started!</p>
+        </div>
+      );
   }
 };
 
