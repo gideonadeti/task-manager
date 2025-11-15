61b09522b5d22cfc9f67201dea2d4157e6b5a65e
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-15 19:46:22 +0000

    style: update Delete button styling for improved visibility and consistency

--- src/app/components/TaskActions.tsx
+++ src/app/components/TaskActions.tsx
@@ -44,7 +44,7 @@ export default function TaskActions({ task }: TaskActionsProps) {
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => setDeleteOpen(true)}
-          className="text-destructive bg-destructive/10"
+          className="text-red-500 hover:bg-red-500/10"
         >
           Delete
         </DropdownMenuItem>
