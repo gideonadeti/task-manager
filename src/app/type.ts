e98919181c024efe04cfcd38499f80161fde9721
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-15 09:21:50 +0000

    refactor: remove ExtendedGroup and ExtendedTask interfaces to clean up type definitions

--- src/app/type.ts
+++
@@ -1,9 +0,0 @@
-import { Group, Task } from "@prisma/client";
-
-export interface ExtendedGroup extends Group {
-  tasks: Task[];
-}
-
-export interface ExtendedTask extends Task {
-  group: Group;
-}
