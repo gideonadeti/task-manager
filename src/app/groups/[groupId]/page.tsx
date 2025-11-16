48569669be65f8219592d493a0e556b2f3c71cca
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-16 08:35:25 +0000

    style: adjust padding in GroupPage component for improved layout consistency

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -82,7 +82,7 @@ export default function GroupPage() {
   }
 
   return (
-    <div className="flex-1 px-4 sm:px-6 lg:px-8 py-4 sm:py-6">
+    <div className="flex-1 px-2 sm:px-4 lg:px-6 py-2 sm:py-4">
       {filteredTasks.length > 0 ? (
         <TasksList data={filteredTasks} />
       ) : (
