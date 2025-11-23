5c297263e35d4a193c6f9bb2dc9a77926abc4385
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-23 21:34:00 +0000

    fix: update text in NotFound component to use HTML entities for apostrophes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -19,8 +19,8 @@ export default function NotFound() {
           </EmptyMedia>
           <EmptyTitle>Page Not Found</EmptyTitle>
           <EmptyDescription>
-            The page you're looking for doesn't exist or has been moved. Let's
-            get you back on track.
+            The page you&apos;re looking for doesn&apos;t exist or has been
+            moved. Let&apos;s get you back on track.
           </EmptyDescription>
         </EmptyHeader>
         <div className="flex gap-2">
@@ -35,4 +35,3 @@ export default function NotFound() {
     </div>
   );
 }
-
