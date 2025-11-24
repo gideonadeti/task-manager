90bc7bea375c551902f7cf1d51cd4c8ed3a38b43
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-24 08:20:58 +0000

    feat: wrap AppSidebar in Suspense for improved loading handling in group layout

--- src/app/groups/[groupId]/layout.tsx
+++ src/app/groups/[groupId]/layout.tsx
@@ -1,16 +1,19 @@
+import { Suspense } from "react";
+
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-
 import Header from "@/app/components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main className="flex-grow flex flex-col min-h-svh">
-        <Header />
-        {children}
-      </main>
+      <Suspense fallback={null}>
+        <AppSidebar />
+        <main className="flex-grow flex flex-col min-h-svh">
+          <Header />
+          {children}
+        </main>
+      </Suspense>
     </SidebarProvider>
   );
 }
